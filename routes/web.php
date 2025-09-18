2294d38fd5d90e94e0b7dd87f308203722f5550b
Bart Kouwenberg <124251111+BartK28@users.noreply.github.com>
2025-09-18 12:09:21 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/weer/{country}/{city}', [App\Http\Controllers\WeatherController::class, 'getWeather'])->name('weather.index');
 Route::get('/currencyconverter/{from}/{to}/{amount}', [App\Http\Controllers\CurrencyController::class, 'getCurrency'])->name('currency.index');
+Route::get('/currencyconverter', [App\Http\Controllers\CurrencyController::class, 'getCurrencies'])->name('currency');
 Route::get('/quote', [App\Http\Controllers\QuoteController::class, 'getQuote'])->name('quote.index');
